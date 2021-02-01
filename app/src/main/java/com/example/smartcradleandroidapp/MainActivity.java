36b4a35a8df5481eacc59189cbddcab851a2b360
Senduran Ravikumar <sendurangr@users.noreply.github.com>
2021-02-01 23:59:49 +0530

    SplashScreen Tried

--- app/src/main/java/com/example/smartcradleandroidapp/MainActivity.java
+++ app/src/main/java/com/example/smartcradleandroidapp/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.smartcradleandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getSupportActionBar().hide();
+
+        final Handler handler = new Handler(Looper.getMainLooper());
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        }, 3000);
     }
 }
