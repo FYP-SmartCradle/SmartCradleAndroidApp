20ffae5761cf9f20e2079152a103a4296941b425
Senduran Ravikumar <sendurangr@users.noreply.github.com>
2021-02-06 11:03:14 +0530

    Foreground services implementation Tested

--- app/src/main/java/com/example/smartcradleandroidapp/HomeActivity.java
+++ app/src/main/java/com/example/smartcradleandroidapp/HomeActivity.java
@@ -1,11 +1,20 @@
 package com.example.smartcradleandroidapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.example.smartcradleandroidapp.service.CradleService;
+
 public class HomeActivity extends AppCompatActivity {
+
+    private static final String TAG = "HomeActivity";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,11 +24,20 @@ public class HomeActivity extends AppCompatActivity {
 
 
     public void startService(View view){
-        System.out.println("start service button works fine.");
+        String input = "This is Cradle Service integration";
+        Log.v(TAG,"Service Started from home");
+
+        Intent serviceIntent = new Intent(this, CradleService.class);
+        serviceIntent.putExtra("inputExtra",input);
+        ContextCompat.startForegroundService(this, serviceIntent);
+        Log.v(TAG,"Service Started intentService");
+
     }
 
     public void stopService(View view){
-        System.out.println("start service button works fine.");
+        Intent serviceIntent = new Intent(this, CradleService.class);
+        stopService(serviceIntent);
+        Log.v(TAG,"Service Stopped from home");
     }
 
 }
--- app/src/main/java/com/example/smartcradleandroidapp/service/CradleService.java
+++ app/src/main/java/com/example/smartcradleandroidapp/service/CradleService.java
@@ -1,12 +1,22 @@
 package com.example.smartcradleandroidapp.service;
 
+import android.app.Notification;
+import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 import androidx.annotation.Nullable;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+
+import com.example.smartcradleandroidapp.HomeActivity;
+import com.example.smartcradleandroidapp.MainActivity;
 
 public class CradleService extends Service {
+
+    private static final String TAG = "CradleService";
 
     @Override
     public void onCreate() {
@@ -17,7 +27,22 @@ public class CradleService extends Service {
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
         //will be triggered when we start service
-        return super.onStartCommand(intent, flags, startId);
+        Log.v(TAG,"Service Started in onStartCommand");
+
+        String input = intent.getStringExtra("inputExtra");
+
+        Intent notificationIntent = new Intent(this, MainActivity.class);
+        PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent, 0);
+
+        Notification notification = new NotificationCompat.Builder(this, "ServiceNotification")
+                .setContentTitle("Service")
+                .setContentText("Foreground Service")
+                .setContentIntent(pendingIntent)
+                .build();
+
+        startForeground(1,notification);
+
+        return START_NOT_STICKY;
     }
 
     @Override
