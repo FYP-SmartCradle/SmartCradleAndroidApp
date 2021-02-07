f6c5a1395010a42b8df2dab631167d632e53cac2
Senduran Ravikumar <sendurangr@users.noreply.github.com>
2021-02-07 15:40:43 +0530

    Theme Tested

--- app/src/main/java/com/example/smartcradleandroidapp/HomeActivity.java
+++ app/src/main/java/com/example/smartcradleandroidapp/HomeActivity.java
@@ -1,43 +1,99 @@
 package com.example.smartcradleandroidapp;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.content.ContextCompat;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.content.Context;
 import android.content.Intent;
+import android.content.res.Configuration;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Gravity;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.Toolbar;
 
 import com.example.smartcradleandroidapp.service.CradleService;
+import com.google.android.material.appbar.MaterialToolbar;
+import com.google.android.material.internal.NavigationMenu;
+import com.google.android.material.internal.NavigationSubMenu;
+import com.google.android.material.navigation.NavigationView;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private static final String TAG = "HomeActivity";
+
+    private DrawerLayout drawer;
+    private NavigationView navigationView;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+
+        drawer = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.nav_view);
+
+        ActionBarDrawerToggle actionBarDrawerToggle = new ActionBarDrawerToggle(
+                this,
+                drawer,
+                toolbar,
+                R.string.navigation_drawer_open,
+                R.string.navigation_drawer_close
+        );
+
+        drawer.addDrawerListener(actionBarDrawerToggle);
+        actionBarDrawerToggle.syncState();
+        navigationView.setNavigationItemSelectedListener(this);
+
+
     }
 
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 
-    public void startService(View view){
+    public void startService(View view) {
         String input = "This is Cradle Service integration";
-        Log.v(TAG,"Service Started from home");
+        Log.v(TAG, "Service Started from home");
 
         Intent serviceIntent = new Intent(this, CradleService.class);
-        serviceIntent.putExtra("inputExtra",input);
+        serviceIntent.putExtra("inputExtra", input);
         ContextCompat.startForegroundService(this, serviceIntent);
-        Log.v(TAG,"Service Started intentService");
+        Log.v(TAG, "Service Started intentService");
 
     }
 
-    public void stopService(View view){
+    public void stopService(View view) {
         Intent serviceIntent = new Intent(this, CradleService.class);
         stopService(serviceIntent);
-        Log.v(TAG,"Service Stopped from home");
+        Log.v(TAG, "Service Stopped from home");
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        return false;
+    }
+
+    @Override
+    public void onPointerCaptureChanged(boolean hasCapture) {
+
     }
 
 }
