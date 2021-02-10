6d94ba76a0631c48dc8bfd538b7186958e5fa2b4
Senduran Ravikumar <sendurangr@users.noreply.github.com>
2021-02-10 23:35:33 +0530

    settings activity

--- app/src/main/java/com/example/smartcradleandroidapp/HomeActivity.java
+++ app/src/main/java/com/example/smartcradleandroidapp/HomeActivity.java
@@ -4,17 +4,15 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
-import android.view.View;
 
+import androidx.annotation.DrawableRes;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
-import androidx.core.content.ContextCompat;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
 
-import com.example.smartcradleandroidapp.service.CradleService;
+import com.example.smartcradleandroidapp.user_interfaces.settings.SettingsActivity;
 import com.google.android.material.navigation.NavigationView;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -36,7 +34,6 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
 
         drawer = findViewById(R.id.drawer_layout);
         navigationView = findViewById(R.id.nav_view);
@@ -53,7 +50,6 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         actionBarDrawerToggle.syncState();
         navigationView.setNavigationItemSelectedListener(this);
 
-        this.startForegroundService();
     }
 
     @Override
@@ -66,33 +62,6 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     }
 
 
-    public void startForegroundService() {
-        String input = "This is Cradle Service integration";
-        Log.v(TAG, "Service Started from home");
-
-        Intent serviceIntent = new Intent(this, CradleService.class);
-        serviceIntent.putExtra("inputExtra", input);
-        ContextCompat.startForegroundService(this, serviceIntent);
-        Log.v(TAG, "Service Started intentService");
-    }
-
-    public void startService(View view) {
-        String input = "This is Cradle Service integration";
-        Log.v(TAG, "Service Started from home");
-
-        Intent serviceIntent = new Intent(this, CradleService.class);
-        serviceIntent.putExtra("inputExtra", input);
-        ContextCompat.startForegroundService(this, serviceIntent);
-        Log.v(TAG, "Service Started intentService");
-
-    }
-
-    public void stopService(View view) {
-        Intent serviceIntent = new Intent(this, CradleService.class);
-        stopService(serviceIntent);
-        Log.v(TAG, "Service Stopped from home");
-    }
-
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         Log.v(TAG, "On Navigation Click" + item.getTitle());
@@ -100,7 +69,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
         switch (itemTitle) {
             case "Setting":
-                Intent intent = new Intent(this, AmbientInspectionActivity.class);
+                Intent intent = new Intent(this, SettingsActivity.class);
                 startActivity(intent);
         }
 
---
+++ app/src/main/java/com/example/smartcradleandroidapp/user_interfaces/settings/SettingsActivity.java
@@ -0,0 +1,102 @@
+package com.example.smartcradleandroidapp.user_interfaces.settings;
+
+import android.app.ActivityManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.core.content.ContextCompat;
+
+import com.example.smartcradleandroidapp.R;
+import com.example.smartcradleandroidapp.service.CradleService;
+import com.google.android.material.switchmaterial.SwitchMaterial;
+
+
+public class SettingsActivity extends AppCompatActivity {
+
+    private static final String TAG = "SettingsActivity";
+    private SwitchMaterial switchTheme, serviceSwitch;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        ActionBar ab = getSupportActionBar();
+        // Enable the Up button
+        ab.setDisplayHomeAsUpEnabled(true);
+
+        updateSwitchState();
+    }
+
+    private void updateSwitchState() {
+        serviceSwitch = findViewById(R.id.service_switch);
+        switchTheme = findViewById(R.id.theme_switch);
+        if (isDarkThemeActivated()) {
+            switchTheme.setChecked(true);
+        } else {
+            switchTheme.setChecked(false);
+        }
+
+        if (isCradleServiceRunning(CradleService.class)) {
+            serviceSwitch.setChecked(true);
+        } else {
+            serviceSwitch.setChecked(false);
+        }
+
+    }
+
+
+    public void startCradleForegroundService() {
+        String input = "This is Cradle Service integration";
+        Log.v(TAG, "Service Started from home");
+
+        Intent serviceIntent = new Intent(this, CradleService.class);
+        serviceIntent.putExtra("inputExtra", input);
+        ContextCompat.startForegroundService(this, serviceIntent);
+        Log.v(TAG, "Service Started intentService");
+
+    }
+
+    public void stopCradleForegroundService() {
+        Intent serviceIntent = new Intent(this, CradleService.class);
+        stopService(serviceIntent);
+        Log.v(TAG, "Service Stopped from home");
+    }
+
+    public void lookUpServices(View view) {
+        if (serviceSwitch.isChecked()) {
+            startCradleForegroundService();
+        } else {
+            stopCradleForegroundService();
+        }
+    }
+
+    public void switchTheme(View view) {
+        if (switchTheme.isChecked()) {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES);
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO);
+        }
+    }
+
+
+    private boolean isCradleServiceRunning(Class<?> serviceClass) {
+        ActivityManager manager = (ActivityManager) getSystemService(Context.ACTIVITY_SERVICE);
+        for (ActivityManager.RunningServiceInfo service : manager.getRunningServices(Integer.MAX_VALUE)) {
+            if (serviceClass.getName().equals(service.service.getClassName())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isDarkThemeActivated() {
+        return AppCompatDelegate.MODE_NIGHT_YES == AppCompatDelegate.getDefaultNightMode();
+    }
+}
