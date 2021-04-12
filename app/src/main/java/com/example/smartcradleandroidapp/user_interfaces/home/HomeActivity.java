3b5f490246445e69e575ba383851eee44eac63e0
Senduran Ravikumar <sendurangr@users.noreply.github.com>
2021-04-13 00:52:45 +0530

    Ui design started

--- app/src/main/java/com/example/smartcradleandroidapp/user_interfaces/home/HomeActivity.java
+++ app/src/main/java/com/example/smartcradleandroidapp/user_interfaces/home/HomeActivity.java
@@ -1,9 +1,9 @@
 package com.example.smartcradleandroidapp.user_interfaces.home;
 
 import android.annotation.SuppressLint;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.MenuItem;
@@ -11,10 +11,8 @@ import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-import androidx.preference.PreferenceManager;
 
 import com.example.smartcradleandroidapp.R;
 import com.example.smartcradleandroidapp.user_interfaces.assistent.VoiceAssistantActivity;
@@ -61,7 +59,6 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
 
         androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
 
 
         drawer = findViewById(R.id.drawer_layout);
@@ -127,7 +124,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
                 break;
 
 
-            case  R.id.nav_electronics:
+            case R.id.nav_electronics:
                 getSupportFragmentManager()
                         .beginTransaction()
                         .replace(R.id.home_fragment_container, new HomeApplianceFragment())
@@ -171,7 +168,6 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
 
 
     @Override
