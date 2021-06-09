04cfedb2b94f8634374af755ae003ed53a607067
Senduran Ravikumar <sendurangr@users.noreply.github.com>
2021-06-09 19:18:02 +0530

    updated to the newer version

--- app/src/main/java/com/example/smartcradleandroidapp/user_interfaces/settings/SettingsPreferenceActivity.java
+++ app/src/main/java/com/example/smartcradleandroidapp/user_interfaces/settings/SettingsPreferenceActivity.java
@@ -8,6 +8,8 @@ import androidx.fragment.app.Fragment;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.example.smartcradleandroidapp.R;
+
 
 public class SettingsPreferenceActivity extends AppCompatActivity implements
         PreferenceFragmentCompat.OnPreferenceStartFragmentCallback {
