e8745b16794b643072fc9227611cd1d24eb4bb2d
Senduran Ravikumar <sendurangr@users.noreply.github.com>
2021-06-21 00:49:19 +0530

    updated to the newer version

---
+++ app/src/main/java/com/example/smartcradleandroidapp/user_interfaces/alert/AlertIndicatorActivity.java
@@ -0,0 +1,16 @@
+package com.example.smartcradleandroidapp.user_interfaces.alert;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.smartcradleandroidapp.R;
+
+public class AlertIndicatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alert_indicator);
+    }
+}
